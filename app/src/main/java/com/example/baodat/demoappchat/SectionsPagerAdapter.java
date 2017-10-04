8da2664a9861773174dd024149e66741539c0380
BaoDat <baodat96@gmail.com>
2017-10-04 12:41:13 +0700

    first commit

---
+++ app/src/main/java/com/example/baodat/demoappchat/SectionsPagerAdapter.java
@@ -0,0 +1,10 @@
+package com.example.baodat.demoappchat;
+
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Bao Dat on 04/10/2017.
+ */
+
+class SectionsPagerAdapter extends FragmentPagerAdapter{
+}
